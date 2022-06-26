da315ebea09c03fdfd6a25bc6f35453895dcc3eb
Dereje Desta <ddesta777@gmail.com>
2022-06-26 12:46:51 +0300

    handled IndexError and added additional tests

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,11 +13,23 @@ def canUnlockAll(boxes):
 
     >>> canUnlockAll([[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]])
     False
+
+    >>> canUnlockAll([[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]])
+    True
+
+    >>> canUnlockAll([[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]])
+    False
+
+    >>> canUnlockAll([[0]])
+    True
     """
     opened = set()
     opened.add(0)
     for i in range(len(boxes)):
         for j in opened.copy():
-            opened.update(boxes[j])
+            try:
+                opened.update(boxes[j])
+            except IndexError:
+                pass
 
     return set(range(len(boxes))).issubset(opened)
