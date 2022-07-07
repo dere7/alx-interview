0282dd75854daf0b9b3f39eabcd1570ac9948d3d
Dereje Desta <ddesta777@gmail.com>
2022-07-07 12:37:28 +0300

    handled keyboard interrupt in project 0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 """reads stdin line by line and computes metrics"""
+import sys
 
 
-def print_stat(status_codes, tot_file_size):
+def print_stat(tot_file_size, codes):
     """print stat for specified inputs"""
-    print('File size: {}'.format(total_file_size))
-    for k, v in sorted(status_code_counts.items(), key=lambda i: i[0]):
-        if v != 0:
-            print('{}: {}'.format(k, v))
+    print("File size: {}".format(tot_file_size))
+    codes_sorted = sorted(codes.keys())
+    for code in codes_sorted:
+        if codes[code] > 0:
+            print("{}: {}".format(code, codes[code]))
 
 
-if __name__ == '__main__':
-    status_codes = [400, 200, 301, 401, 403, 404, 405, 500]
-    status_code_counts = {str(status): 0 for status in status_codes}
-    total_file_size = 0
-    line_count = 0
-    while True:
-        try:
-            line = input()
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+status_code_counts = {str(status): 0 for status in status_codes}
+total_file_size = 0
+line_count = 0
+
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            status_code, file_size = line.split()[-2:]
+            if status_code in status_code_counts.keys():
+                status_code_counts[status_code] += 1
+            file_size = int(file_size)
+            total_file_size += file_size
             line_count += 1
-            status_code, file_size = line.split(' ')[-2:]
-            if int(status_code) not in status_codes:
-                continue
-            status_code_counts[status_code] += 1
-            total_file_size += int(file_size)
             if line_count == 10:
-                print_stat(status_code_counts, total_file_size)
-                # reset after printing
-                status_code_counts = {
-                    str(status): 0 for status in status_codes}
-                total_file_size = 0
+                print_stat(total_file_size, status_code_counts)
                 line_count = 0
-        except KeyboardInterrupt:
-            print_stat(status_code_counts, total_file_size)
-            exit(1)
+    except KeyboardInterrupt:
+        print_stat(total_file_size, status_code_counts)
+        raise
+    except Exception:
+        pass
+    print_stat(total_file_size, status_code_counts)
